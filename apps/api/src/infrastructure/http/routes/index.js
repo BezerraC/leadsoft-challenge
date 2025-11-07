c9480ca32146090603fe0e66896d8834e61c20fd
BezerraC <cbezerraneto@gmail.com>
2025-11-07 03:22:32 -0300

    feat: admin routes

--- apps/api/src/infrastructure/http/routes/index.js
+++ apps/api/src/infrastructure/http/routes/index.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const candidateRoutes = require('./candidate.routes');
-// const adminRoutes = require('./admin.routes');
+const adminRoutes = require('./admin.routes');
 
 const router = Router();
 
 // Rotas de candidatos
 router.use('/candidates', candidateRoutes);
 
-// router.use('/admin', adminRoutes);
+// Rotas de admin
+router.use('/admin', adminRoutes);
 
 module.exports = router;
