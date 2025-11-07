f17bde815958a6be83ab1c24cf60dabc22aab022
BezerraC <cbezerraneto@gmail.com>
2025-11-07 15:11:45 -0300

    style: new style from leadsoft identity

--- apps/web/app/layout.js
+++ apps/web/app/layout.js
@@ -1,8 +1,25 @@
-import { Inter } from 'next/font/google';
 import './globals.css';
+import { Orbitron, Roboto, Roboto_Mono } from 'next/font/google';
 import { RecaptchaProvider } from '../providers/RecaptchaProvider';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
-const inter = Inter({ subsets: ['latin'] });
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-orbitron",
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-roboto",
+});
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-roboto-mono",
+});
 
 export const metadata = {
   title: 'LeadSoft Challenger',
@@ -12,9 +29,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`min-h-screen flex flex-col bg-gray-950 ${orbitron.variable} ${roboto.variable} ${robotoMono.variable}`}>
         <RecaptchaProvider>
-          {children}
+          <Header />
+          <main className="flex-grow">
+            {children}
+          </main>
+          <Footer />
         </RecaptchaProvider>
       </body>
     </html>
